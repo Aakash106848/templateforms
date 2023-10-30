e1fc49959e2dc9b5a141129be241a1837d9a7da4
Aakash106848 <aakash.b@gavstech.com>
2023-10-30 10:20:08 +0530

    Project Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateforms';
+  detailsArray:any[] = [];
+  enable: boolean = false;
+  getuserarray:any[] = [];
+
+  addUser(value: any){
+    
+    console.log(value);
+    
+    this.detailsArray.push(value);
+  }
+  deleteUser(user: any){
+    this.detailsArray.splice(this.detailsArray.indexOf(user),1);
+  }
+  UpdateUser(user: any){
+    const updatedUser = this.detailsArray.find(x => x.firstName === user.firstName);
+    updatedUser.lastName = user.lastName;
+    updatedUser.email = user.email;
+   
+    updatedUser.salary = user.salary; 
+  }
+  hideshow()
+  {
+    this.enable = !this.enable;
+  } 
+  getUser(user: any)
+  {
+    this.getuserarray.pop();
+    const x = this.detailsArray.find(x => x.firstName === user.firstName);
+    this.getuserarray.push(x);
+  }
+
 }
